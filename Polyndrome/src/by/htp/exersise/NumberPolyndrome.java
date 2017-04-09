25706ce122ae9fe5126543459c07fe88fbef4197
Stas Kleinikov <Stasik666_89@mail.ru>
2017-04-09 21:53:13 +0300

    I do not understand how exactly I need to create javadoc

---
+++ Polyndrome/src/by/htp/exersise/NumberPolyndrome.java
@@ -0,0 +1,39 @@
+package by.htp.exersise;
+
+/** I got the assignment : 06.04.2017 21.20 
+* The assignment is complete : 09.04.2017 21.15
+* @author - Kleinikov Stanislav
+*/ 
+
+public class NumberPolyndrome {
+	public static void main(String[] args) {
+
+		int [] someInt = {1124211,333221,1123213,653809};
+		int countPalindrom = 0;
+		int numberPalindrom = 0;
+		for(int i = 0; i<someInt.length;i++){
+			if(palindrom(String.valueOf(someInt[i])) == true){
+				countPalindrom++;
+				numberPalindrom = someInt[i];
+				if(countPalindrom == 2){
+					System.out.println("Second palindrom: " + someInt[i]);
+					break;
+				}
+			}
+		}
+		if(countPalindrom<2){
+			System.out.println("Palindrom: " + numberPalindrom);
+		}
+		
+	}
+	
+    private static boolean palindrom(String str) {
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+}
+
+/**
+*@author - Kleinikov
+*I got the assignment : 06.04.2017 21.20
+*The assignment is complete : 09.04.2017 21.15
+*/
